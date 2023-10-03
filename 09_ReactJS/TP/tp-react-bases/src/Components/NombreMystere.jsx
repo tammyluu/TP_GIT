a1fcf8f482d885e5328599283d7fd6ea8810538a
unknown <luutam_viva@yahoo.com>
2023-10-03 17:26:55 +0200

    mystery

--- 09_ReactJS/TP/tp-react-bases/src/Components/NombreMystere.jsx
+++ 09_ReactJS/TP/tp-react-bases/src/Components/NombreMystere.jsx
@@ -1,3 +1,22 @@
 import { useEffect, useState } from "react";
 
-const 
+const MysteryNumber = (myMysteryNumber)=>{
+    const [inputA, setInputA] = useState("")
+    const [inputB, setInputB] = useState("")
+    const [isRightInput, setRightInput] = useState(false)
+    const inputAHandle = (event) => {
+        setInputA(event.target.value)
+    }
+    const inputBHandle = (event) => {
+        setInputB(event.target.value)
+    }
+    return (
+        <>
+        <input type="text" value={inputA} onInput={inputAHandle} />
+        <input type="text" value={inputB} onInput={inputBHandle} />
+        <p>My mystery number : <b>{isRightInput ? 'You win!' : 'Try again!'}</b> </p>
+        </>
+    )
+
+}
+export default MysteryNumber
