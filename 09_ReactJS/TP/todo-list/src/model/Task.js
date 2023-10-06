16516d53bc12454ad4358c523e1a8c43daa4c422
ST_Tammy <104438570+tammyluu@users.noreply.github.com>
2023-10-07 00:34:38 +0200

    TP todo list

---
+++ 09_ReactJS/TP/todo-list/src/model/Task.js
@@ -0,0 +1,22 @@
+class Task {
+    static count = 0
+    constructor (_id, _name, _deadline, _isFinish){
+        this._id = Task.count + 1
+        this._name = _name
+        this._deadline = _deadline
+        this._isFinished = false
+    }
+    get id(){
+        return this._id
+    }    
+    get name (){
+        return this._name
+    }
+    get deadline (){
+        return this._deadline
+    }
+    get isFinished (){
+        return this._isFinished
+    }
+}
+export default Task
