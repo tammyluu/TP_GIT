85d05e9d2f4d18782fab7c2aa8c0a0c831a802eb
unknown <luutam_viva@yahoo.com>
2023-11-08 17:19:42 +0100

    exo

---
+++ 14_ReactNative/TP/listdecourses/App.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { create } from 'react-test-renderer'
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './screens/Home'
+import AddDisplay from './screens/AddDisplay'
+import DetailList from './screens/DetailList'
+
+const Stack = create.StackNavigator()
+
+export default function App() {
+  return (
+   <NavigationContainer>
+        <Stack.Navigator initialRouteName={Home}>
+            <Stack.Screen name='Home' component={Home} />
+            <Stack.Screen name='AddDisplay' component={AddDisplay} options={{title: "Add courses"}}/>
+            <Stack.Screen name='DetailList' component={DetailList} options={{title: "Detail"}}/>
+        </Stack.Navigator>
+   </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ 14_ReactNative/TP/listdecourses/screens/DetailList.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function DetailList() {
+  return (
+    <View>
+      <Text>DetailList</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
