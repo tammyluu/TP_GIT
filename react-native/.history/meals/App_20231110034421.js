19bd8f1f2a6df88f37d8c20abbdb0ff026a9383b
ST_Tammy <104438570+tammyluu@users.noreply.github.com>
2023-11-10 09:38:42 +0100

    structure of proj

---
+++ react-native/.history/meals/App_20231110034421.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.myText}>Coucou!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  myText:{
+    font
+  }
+});
